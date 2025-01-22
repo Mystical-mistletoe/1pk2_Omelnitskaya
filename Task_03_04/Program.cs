4cab9128b268b684a939cf497806489e06ddca9a
Екатерина Омельницкая <k625972@gmail.com>
2025-01-22 21:28:40 +0500

    решена 4 задача 3 урока

---
+++ Task_03_04/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Task_03_04
+{
+    internal class Program
+    {
+        /*Пользователь вводить в консоли произвольный текст.
+         * После каждого ввода консоль очищается. Когда
+         * пользователь вводить слово «exit» или пустую 
+         * строку – ввод останавливается и выводиться 
+         * количество строк введенных пользователем
+         */
+
+        static void Main(string[] args)
+        {
+            int a = 0;
+            string customText;
+            Console.WriteLine("вводите текст, для остановки ввода введите слово «exit» или пустую строку");
+            do
+            {
+                customText = Console.ReadLine();
+                a++;
+                Console.Clear();
+            }
+            while (customText != "exit" && customText != " ");
+            Console.WriteLine($"вы ввели {a-1} строк(у)");
+        }
+    }
+}
