1b74a3b62092c87b6754b1be52dc24d20487e2f9
Екатерина Омельницкая <k625972@gmail.com>
2025-01-22 20:50:34 +0500

    решена задача 1 из урока 3

---
+++ Task_03_01/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Task_03_01
+{
+    internal class Program
+    {
+        /*Выведите классическую таблицу умножения 
+        * используя вложенные циклы
+        */
+
+        static void Main(string[] args)
+
+        {
+
+            for (int i = 2; i <= 10; i++) //внешний цикл
+            {
+                Console.WriteLine($"умножение на {i}");
+
+                for (int j = 2; j <= 10; j++) //внутренний цикл
+                {
+                    Console.WriteLine($"{i} * {j} = {i * j}"); //интеполированный вывод
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
