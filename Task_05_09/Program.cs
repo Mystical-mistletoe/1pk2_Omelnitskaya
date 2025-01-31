5bc55ab0ef09be1c1a48f42f676042f24b3357f2
Екатерина Омельницкая <k625972@gmail.com>
2025-01-31 19:57:13 +0500

    решена задача 9 из урока 5

---
+++ Task_05_09/Program.cs
@@ -0,0 +1,84 @@
+﻿namespace Task_05_09
+{
+    internal class Program
+    {
+        /*Дан квадратный массив размерность n*n. 
+         * Произведите анализ данной матрицы и 
+         * выясните является ли данная матрица Z-матрицей
+         * (это матрица, где все недиагональные элементы 
+         * меньше нуля)
+         * Если данное условие выполняется то вывести 
+         * данную матрицу с цветовой индикацией главной 
+         * диагонали. Если не выполняется, то
+         * вывести сообщение что данная матрица 
+         * не является Z-матрицей
+         */
+        static void Main(string[] args)
+        {
+            int[,] numbers = {
+                { 1,-1,-1,-1, 1},
+                { -1, 1, -1, 1, -1},
+                { -1, - 1, 1, -1, -1},
+                { -1, 1, -1, 1, -1},
+                { 1, - 1, -1, - 1, 1},
+            };
+            bool flag = false;
+
+            //заполнение массива - для проверки других вариантов
+            /*
+            Console.WriteLine("Введите размер квадратной матрицы:");
+            int n = Int32.Parse(Console.ReadLine());
+
+            int[,] numbers = new int[n, n];
+            Random random = new Random();
+            for (int i = 0; i < numbers.GetLength(0); i++)
+            {
+                for (int j = 0; j < numbers.GetLength(1); j++)
+                {
+                    numbers[i, j] = random.Next(-1, 2);
+                    Console.Write(numbers[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            */
+
+            for (int i = 0; i < numbers.GetLength(0); i++)
+            {
+                for (int j = 0; j < numbers.GetLength(1); j++)
+                {
+                    if (numbers[i, j] >= 0 && i != j && i != numbers.GetLength(0) - j - 1)
+                    {
+                        flag = true;
+                        break;
+                    }
+                    if (numbers[i, j] <= 0 && (i == j || i == numbers.GetLength(0)- j - 1))
+                    {
+                        flag = true;
+                        break;
+                    }
+                }
+            }
+            if (flag)
+            {
+                Console.WriteLine("матрица не является Z-матрицей");
+            }
+            else
+            {
+                Console.WriteLine("вывод матрицы с выделением главной диагонали:");
+                for (int i = 0; i < numbers.GetLength(0); i++)
+                {
+                    for (int j = 0; j < numbers.GetLength(0); j++)
+                    {
+                        if (i == j)
+                        {
+                            Console.BackgroundColor = ConsoleColor.Green;
+                        }
+                        Console.Write(numbers[i, j] + " ");
+                        Console.BackgroundColor = ConsoleColor.Black;
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
