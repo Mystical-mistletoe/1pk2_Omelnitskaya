5673fb88015f505f48cc2c44b0452fadfe5c1470
Екатерина Омельницкая <k625972@gmail.com>
2025-03-05 19:35:42 +0500

    solved 6 task of the 10th lesson

---
+++ Task_10_06/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Task_10_06
+{
+    internal class Program
+    {
+        /*Создать Метод ArrayGeneration не возвращающий 
+         * значения, принимает целое число n, выводит 
+         * на консоль сгенерированный массив размерности n*n.
+        */
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Введите целое число для генерации массива:");
+                int n = Convert.ToInt32(Console.ReadLine());
+
+                ArrayGeneration(n);
+            }
+
+        }
+        /// <summary>
+        /// генерация двумерного массива размерности n*n
+        /// </summary>
+        /// <param name="n">целое число, размер квадратного массива</param>
+        /// <returns></returns>
+        static void ArrayGeneration(int n)
+        {
+            Random random = new Random();
+            int[,] array = new int[n, n];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    array[i, j] = random.Next(-99, 100);
+                    Console.Write(array[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(array[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
