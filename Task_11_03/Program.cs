94024706d8fb389d9d3613e14b499ce1b1495bc4
Екатерина Омельницкая <k625972@gmail.com>
2025-03-09 13:19:33 +0500

    solved task 3 from the 11th lesson

---
+++ Task_11_03/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Task_11_03
+{
+    internal class Program
+    {
+        /*Выходной параметр (out): 
+         * Напишите метод, который принимает строку 
+         * и возвращает через выходной параметр 
+         * количество гласных и согласных букв в этой строке
+        */
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку");
+            string strochka = Console.ReadLine();
+            int countVowels, countConsonats;
+
+            VowelsConsonants(strochka, out countVowels, out countConsonats);
+
+            Console.WriteLine($"Количество гласных: {countVowels}");
+            Console.WriteLine($"Количество согласных: {countConsonats}");
+            Console.WriteLine("ъ и ь не относятся");
+
+        }
+        /// <summary>
+        /// подсчитывает колво гл и согл букв в строке
+        /// </summary>
+        /// <param name="cVowels">целое число 1</param>
+        /// /// <param name="cConsonats">целое число 2</param>
+        /// <returns>количество гласных и согласных букв</returns>
+        static void VowelsConsonants(string strochka, out int cVowels, out int cConsonats)
+        {
+            string vowels = "аеёиоуыэюяАЕЁИОУЫЭЮЯ";
+            string consonats = "йцкнгшщзхфвпрлджчсмтбЙЦКНГШЩЗХЪФВПРЛДЖЧСМТЬБ";
+            cVowels = 0;
+            cConsonats = 0;
+
+            for (int i = 0; i < strochka.Length; i++)
+            {
+                if (vowels.Contains(strochka[i].ToString()))
+                {
+                    cVowels++;
+                }
+                else if (consonats.Contains(strochka[i].ToString()))
+                {
+                    cConsonats++;
+                }
+            }
+        }
+    }
+}
