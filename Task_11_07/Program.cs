cd4d8b915083111a2768857cea65edd1c10ed155
Екатерина Омельницкая <k625972@gmail.com>
2025-03-09 19:34:32 +0500

    решена 7 задача из урока 11

---
+++ Task_11_07/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace Task_11_07
+{
+    internal class Program
+    {
+        /*Передача массива по ссылке (ref): 
+         * Напишите метод, который принимает массив 
+         * целых чисел по ссылке и изменяет его элементы, 
+         * увеличивая каждый на 1. Проверьте, изменился ли
+         * оригинальный массив вне метода
+        */
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите размер массива");
+            int n = Convert.ToInt32(Console.ReadLine());
+            double[] numbers = new double[n];
+
+            Random random = new Random();
+            Console.WriteLine($"Изначальный массив:");
+
+            for (int i = 0; i < n; i++)
+            {
+                numbers[i] = random.Next(0, 99);
+                Console.Write(numbers[i] + " ");
+            }
+            Console.WriteLine();
+
+            EnlargedArray(ref numbers);
+
+            Console.WriteLine($"Проверка изначального массива:");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(numbers[i] + " ");
+            }
+
+        }
+        /// <summary>
+        /// изменяет массив на +1
+        /// </summary>
+        /// <param name="numbers">массив чисел</param>
+        /// <returns></returns>
+        static void EnlargedArray(ref double[] num)
+        {
+            Console.WriteLine("Измененный массив:");
+            for (int i = 0; i < num.Length; i++)
+            {
+                num[i]++;
+            }
+            for (int i = 0; i < num.Length; i++)
+            {
+                Console.Write(num[i] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
