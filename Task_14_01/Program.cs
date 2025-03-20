62ae7c66d09f79d1ebd7215361c1ea5270d93b9b
Екатерина Омельницкая <k625972@gmail.com>
2025-03-20 09:11:58 +0500

    done

--- Task_14_01/Program.cs
+++ Task_14_01/Program.cs
@@ -2,9 +2,23 @@
 {
     internal class Program
     {
+        /* Создайте произвольный класс, который будет иметь 
+         * статическую переменную count, чтобы отслеживать 
+         * количество экземпляров класса. Каждый раз, 
+         * когда вы создаете новый экземпляр класса, 
+         * увеличивайте count на 1. Создайте метод GetCount(), 
+         * возвращающий текущее значение count.
+        */
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Item i1 = new Item();
+            Item i2 = new Item("ромашка");
+            Item i3 = new Item(120, "sunflower");
+            Item i = new Item();
+            Item.GetItemsCount();
+
+            i3.Dispose();
+            Item.GetItemsCount();
         }
     }
 }
