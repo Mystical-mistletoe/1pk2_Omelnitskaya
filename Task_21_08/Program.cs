baac20a0f255f647c86a0def8cf6cfaa399e7789
Екатерина Омельницкая <k625972@gmail.com>
2025-04-07 19:33:29 +0500

    done 8

---
+++ Task_21_08/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Task_21_08
+{
+    internal class Program
+    {
+        /*напишите метод, который на 
+         * вход получается массив параметров 
+         * (строк) и возвращает 
+         * только уникальные строки
+         * */
+        static void Main(string[] args)
+        {
+            string[] input = { "Диадема", "Кубок", "Кольцо", "Змея", "Кольцо", "Дневник", "Кубок" };
+            string[] end = StringArray.GetUniqueStrings(input);
+
+            foreach (string s in end)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
---
+++ Task_21_08/StringArray.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_21_08
+{
+    internal class StringArray
+    {
+        public static string[] GetUniqueStrings(string[] inputArray)
+        {
+            List<string> uniqueStrings = new List<string>();
+            HashSet<string> seenStrings = new HashSet<string>();
+
+            foreach (string str in inputArray)
+            {
+                if (!seenStrings.Contains(str)) //Если не встречалась
+                {
+                    uniqueStrings.Add(str);
+                    seenStrings.Add(str);
+                }
+            }
+
+            return uniqueStrings.ToArray();
+        }
+    }
+}
