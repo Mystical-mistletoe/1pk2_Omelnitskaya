41375f711c80ea3ce5480ca46e5adb354edb6f17
Екатерина Омельницкая <k625972@gmail.com>
2025-04-12 02:34:32 +0500

    решено

--- Task_24_08/Program.cs
+++ Task_24_08/Program.cs
@@ -2,9 +2,33 @@
 {
     internal class Program
     {
+        /*реализуйте функцию, осуществляющую 
+         * поиск текста в файле и его замену 
+         * на значения, введенные пользователем
+         */
+        static void ReplaceText(string filePath, string searchText, string replaceText)
+        {
+            if (!File.Exists(filePath))
+            {
+                Console.WriteLine($"Файл '{filePath}' не найден.");
+                return;
+            }
+
+            string fileContent = File.ReadAllText(filePath);
+            string newContent = fileContent.Replace(searchText, replaceText);
+            File.WriteAllText(filePath, newContent);
+            Console.WriteLine("Текст успешно заменен.");
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string filePath = "D:/work/file.txt";
+            Console.Write("Введите текст для поиска: ");
+            string searchText = Console.ReadLine();
+            Console.Write("Введите текст для замены: ");
+            string replaceText = Console.ReadLine();
+
+            ReplaceText(filePath, searchText, replaceText);
         }
     }
 }
