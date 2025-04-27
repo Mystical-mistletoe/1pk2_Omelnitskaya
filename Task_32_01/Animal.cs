8bba5d321e4a9eab53819790feb85ee43b7a8531
Екатерина Омельницкая <k625972@gmail.com>
2025-04-27 23:58:15 +0500

    не добавлено

---
+++ Task_32_01/Animal.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_32_01
+{
+    public abstract class Animal
+    {
+        public string Name { get; } //название животного
+                                    //конструктор, который будет срабатывать в дочерних классах
+                                    //т.к. используется protected
+        protected Animal(string name)
+        {
+            Name = name;
+        }
+        //все животные захотят съесть колобка
+        //но вот получится ли - вопрос...
+        public abstract bool CanEat(Kolobok kolobok);
+    }
+
+    public class Hare : Animal
+    {
+        public Hare() : base("Заяц") { }
+        public override bool CanEat(Kolobok kolobok)
+        {
+            return false; // Заяц не смог съесть Колобка
+        }
+    }
+
+    public class Wolf : Animal
+    {
+        public Wolf() : base("Волк") { }
+        public override bool CanEat(Kolobok kolobok)
+        {
+            return false; // Волк тоже не справился
+        }
+    }
+
+    public class Bear : Animal
+    {
+        public Bear() : base("Медведь") { }
+        public override bool CanEat(Kolobok kolobok)
+        {
+            return false; // Медведь оказался слишком медленным
+        }
+    }
+
+    public class Fox : Animal
+    {
+        public Fox() : base("Лиса") { }
+        public override bool CanEat(Kolobok kolobok)
+        {
+            return true; // Лиса хитрая и съела Колобка
+        }
+    }
+
+
+
+
+
+}
---
+++ Task_32_01/Kolobok.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_32_01
+{
+    public class Kolobok
+    {
+        public int Position { get; private set; }
+        public int Speed { get; }
+        public bool IsAlive { get; private set; }
+        public Kolobok()
+        {
+            Position = 0;
+            Speed = 1;
+            IsAlive = true;
+        }
+        public void Roll()
+        {
+            Position += Speed;
+            Console.WriteLine($"Колобок катится, текущая позиция: {Position}");
+        }
+        //встреча с животным
+        public void MeetAnimal(Animal animal)
+        {
+            if (animal.CanEat(this))
+            {
+                IsAlive = false;
+                Console.WriteLine($"Колобок был съеден {animal.Name}!");
+            }
+            else
+            {
+                Console.WriteLine($"Колобок убежал от {animal.Name}!");
+            }
+        }
+    }
+}
