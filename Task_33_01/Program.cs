ea39ae2f12c6ceb801514e943f85b27a79f2fd3d
Екатерина Омельницкая <k625972@gmail.com>
2025-05-02 00:19:52 +0500

    -

---
+++ Task_33_01/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Task_33_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                List<IDrawable> list = new List<IDrawable>
+                   {
+                       new Circle(5),
+                       new Square(2, 7),
+                       new Circle(10),
+                       new Rectangle(5, 10),
+                       new Rectangle(5, 3)
+                   };
+
+
+                foreach (var item in list)
+                    item.Draw();
+
+                list.Add(new Circle(0));
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ Task_33_01/Rectangle.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_33_01
+{
+    internal class Rectangle : IDrawable
+    {
+        //самостоятельно прописатьтреугольник и его логику рисования в консоли
+        //для упрощения  - прямугольный треугольник
+        private int width;
+        private int height;
+        public int Width
+        {
+            get => width;
+            set
+            {
+                if (value > 1)
+                    width = value;
+                else
+                    throw new ArgumentException("ширина не должна быть меньше или равна 1");
+            }
+        }
+        public int Height
+        {
+            get => height;
+            set
+            {
+                if (value > 1)
+                    height = value;
+                else
+                    throw new ArgumentException("высота не должна быть меньше или равна 1");
+            }
+        }
+
+        public Rectangle(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public void Draw()
+        {
+            Console.WriteLine();
+            for (int i = 1; i < height+1; i++)
+            {
+                for (int j = 1; j < width+1; j++)
+                {
+                    if (j <= (int)(((double)i / height) * width))
+                    {
+                        Console.Write("* ");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
